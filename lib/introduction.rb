3500150bf3908dfa80e1d85cfd6f6e2adb5c55f2
deezy13 <deezy13@gmail.com>
2019-09-04 04:46:05 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,17 +7,17 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language(name, Language)
-  puts "Hi, my name is #{name} and I am learning to program in #{Language}."
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
 
 def introduction_with_language_optional(name, language = "ruby")
-  puts "Hi, my name is #{name} and I am learning to program in #{Language}"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
 
 introduction("Josh")
 introduction_with_language("clyde", "ruby")
-introduction_with_language_optional("Edwin", " ruby")
+introduction_with_language_optional("Edwin", "ruby")
 
 
 
